3d91aeeb27a7fd5d78272f37338f1f1d85bae5ed
Prosenjit Mondal <contact.prosen25@gmail.com>
2024-12-09 17:51:32 +0000

    print day of the week

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,26 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        printDayOfWeek(-1);
+        printDayOfWeek(0);
+        printDayOfWeek(3);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+    }
 
+    public static void printDayOfWeek(int day) {
+
+        String dayOfWeek = switch (day) {
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+
+        System.out.println("Day of the week is " + dayOfWeek);
     }
 }
